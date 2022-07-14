ff1e3709b6655bb3271946d95b1f9b20e67b02eb
Mpisano10 <91083469+Mpisano10@users.noreply.github.com>
2022-07-14 14:33:49 +0200

    Add files via upload

---
+++ esercizio3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+	int num;
+
+	printf("inserisci il tuo numero\n");
+	scanf("%d", &num);
+
+	if(num>=0)
+
+	printf("Il tuo numero è positivo\n");
+
+	else
+	printf("Il tuo numero è negativo\n");
+
+	return 0;
+}
